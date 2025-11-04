9632de56c39ebdfa3dc2c49495fce25aeabcfcb1
etolozadev <estebantoloza1998@gmail.com>
2025-11-04 01:00:50 -0300

    generacion de proyecto base

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+
+    Route::get('quotes', function () {
+        return Inertia::render('quotes');
+    })->name('quotes');
+});
+
+require __DIR__.'/settings.php';
